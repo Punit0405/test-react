f4a9ee59d5867a7c77760488059d2b6cb1f144dc
CodeStar2244 <codestar2244@gmail.com>
2023-02-25 19:11:40 +0530

    tab active done

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -5,54 +5,54 @@ import styles from "./NavbarComponent.module.css"
 
 const NavBarComponent: FunctionComponent = () => {
     return (
-        <Navbar id="navbar" className={styles.navpadding}>
-            <Container>
-                <Nav.Link >
-                    <Link to="/">
-                        <div className={styles.home}>
-                            <i className="fa-regular fa-house setcolor"></i>
-                            <p>Home</p>
-                        </div>
-                    </Link>
-                </Nav.Link>
+        <Nav id="navbar" variant="tabs" className={styles.navpadding}>
+            {/* <Container> */}
+            <Nav.Link eventKey="link-1">
+                <Link to="/">
+                    <div className={styles.home}>
+                        <i className="fa-regular fa-house setcolor"></i>
+                        <p>Home</p>
+                    </div>
+                </Link>
+            </Nav.Link>
 
-                <Nav.Link >
-                    <Link to="/studiomanagement">
-                        <div className={styles.home}>
-                            <i className="fa-regular fa-video setcolor"></i>
-                            <p>Studio Management</p>
-                        </div>
-                    </Link>
-                </Nav.Link>
+            <Nav.Link eventKey="link-2">
+                <Link to="/studiomanagement">
+                    <div className={styles.home}>
+                        <i className="fa-regular fa-video setcolor"></i>
+                        <p>Studio Management</p>
+                    </div>
+                </Link>
+            </Nav.Link>
 
-                <Nav.Link >
-                    <Link to="/gallery">
-                        <div className={styles.home}>
-                            <i className="fa-regular setcolor fa-grid-2"></i>
-                            <p>Gallery</p>
-                        </div>
-                    </Link>
-                </Nav.Link>
+            <Nav.Link eventKey="link-3">
+                <Link to="/gallery">
+                    <div className={styles.home}>
+                        <i className="fa-regular setcolor fa-grid-2"></i>
+                        <p>Gallery</p>
+                    </div>
+                </Link>
+            </Nav.Link>
 
-                <Nav.Link >
-                    <Link to="/asset-registry">
-                        <div className={styles.home}>
-                            <i className="fa-regular fa-folder setcolor"></i>
-                            <p>Asset Registry</p>
-                        </div>
-                    </Link>
-                </Nav.Link>
+            <Nav.Link eventKey="link-4">
+                <Link to="/asset-registry">
+                    <div className={styles.home}>
+                        <i className="fa-regular fa-folder setcolor"></i>
+                        <p>Asset Registry</p>
+                    </div>
+                </Link>
+            </Nav.Link>
 
-                <Nav.Link >
-                    <Link to="/music">
-                        <div className={styles.home}>
-                            <i className="fa-regular fa-circle-play setcolor"></i>
-                            <p >Music</p>
-                        </div>
-                    </Link>
-                </Nav.Link>
-            </Container>
-        </Navbar>
+            <Nav.Link eventKey="link-5">
+                <Link to="/music">
+                    <div className={styles.home}>
+                        <i className="fa-regular fa-circle-play setcolor"></i>
+                        <p >Music</p>
+                    </div>
+                </Link>
+            </Nav.Link>
+            {/* </Container> */}
+        </Nav>
 
     );
 };
