996a5e8e6d029a0cfc01e166af7fa390b0a7c7f9
CodeStar2244 <codestar2244@gmail.com>
2023-02-25 18:10:26 +0530

    navbar done

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -9,31 +9,46 @@ const NavBarComponent: FunctionComponent = () => {
             <Container>
                 <Nav.Link >
                     <Link to="/">
-                        <div className={styles.home}><i className="fa-regular fa-house setcolor"></i>Home</div>
+                        <div className={styles.home}>
+                            <i className="fa-regular fa-house setcolor"></i>
+                            <p>Home</p>
+                        </div>
                     </Link>
                 </Nav.Link>
 
                 <Nav.Link >
                     <Link to="/studiomanagement">
-                        <div className={styles.home}><i className="fa-regular fa-video setcolor"></i>Studio Management</div>
+                        <div className={styles.home}>
+                            <i className="fa-regular fa-video setcolor"></i>
+                            <p>Studio Management</p>
+                        </div>
                     </Link>
                 </Nav.Link>
 
                 <Nav.Link >
                     <Link to="/gallery">
-                        <div className={styles.home}><i className="fa-regular setcolor fa-grid-2"></i>Gallery</div>
+                        <div className={styles.home}>
+                            <i className="fa-regular setcolor fa-grid-2"></i>
+                            <p>Gallery</p>
+                        </div>
                     </Link>
                 </Nav.Link>
 
                 <Nav.Link >
                     <Link to="/collection-setting">
-                        <div className={styles.home}><i className="fa-regular fa-folder setcolor"></i>Asset Registry</div>
+                        <div className={styles.home}>
+                            <i className="fa-regular fa-folder setcolor"></i>
+                            <p>Asset Registry</p>
+                        </div>
                     </Link>
                 </Nav.Link>
 
                 <Nav.Link >
                     <Link to="/music">
-                        <div className={styles.home}><i className="fa-regular fa-circle-play setcolor"></i>Music</div>
+                        <div className={styles.home}>
+                            <i className="fa-regular fa-circle-play setcolor"></i>
+                            <p >Music</p>
+                        </div>
                     </Link>
                 </Nav.Link>
             </Container>
