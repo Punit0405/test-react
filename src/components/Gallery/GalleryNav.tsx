a2c2a22cdcb3dcc44a7865c3492790d63ad2230a
CodeStar2244 <codestar2244@gmail.com>
2023-02-26 14:59:17 +0530

    Modal added

--- src/components/Gallery/GalleryNav.tsx
+++ src/components/Gallery/GalleryNav.tsx
@@ -1,21 +1,23 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import CreateCollectionModal from '../Modal/CreateCollectionModal';
 import styles from "./GalleryNav.module.css";
 
 function GalleryNav() {
+    const [modalShow, setModalShow] = useState(false);
+
     return (
         <>
             <Navbar className={styles.maincomp}>
                 <Container fluid>
                     <Navbar.Brand className={styles.mainname}>Collections</Navbar.Brand>
                     <Form className="d-flex">
-                        <Link to="newcollection">
-                            <Button className={styles.collectionbtn} variant="custom">New Collection</Button>
-                        </Link>
+                        <Button className={styles.collectionbtn} onClick={() => setModalShow(true)} variant="custom">New Collection</Button>
                         <Button className={styles.searchbtn} variant="custom">
                             <i className="fa-regular fa-magnifying-glass"></i>
                         </Button>
@@ -24,6 +26,10 @@ function GalleryNav() {
                         </Button>
                     </Form>
                 </Container>
+                <CreateCollectionModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                />
             </Navbar>
         </>
     );
