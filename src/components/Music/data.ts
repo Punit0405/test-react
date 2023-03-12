330ef900ccaf322a161f83c2a58a64658be06ef7
Vivek Motwani <vivek.motwani0405@gmail.com>
2023-03-12 20:55:35 +0530

    music add

--- src/components/Music/data.ts
+++ src/components/Music/data.ts
@@ -29,5 +29,67 @@ export const musicList = [
         duration:"03:40",
         musicClass:"music-27415"
 
-    }
+    },
+    {
+        src:"https://pagalnew.com/download128/23014",
+        name:'Oo Bolega Ya Oo Oo Bolega',
+        singer: "Kanika Kapoor",
+        duration:"03:46",
+        musicClass:"music-23014"
+    },
+    {
+        src:"https://pagalnew.com/download128/20264",
+        name:'Srivali',
+        singer: "Javed Ali",
+        duration:"03:41",
+        musicClass:"music-20264"
+
+    },
+    {
+        src:"https://pagalnew.com/download128/23015",
+        name:'Saami Saami',
+        singer: "Sunidhi Chauhan",
+        duration:"03:47",
+        musicClass:"music-23015"
+
+    },
+    {
+        src:"https://pagalnew.com/download128/27415",
+        name:'Duppata',
+        singer: "Diesby, Shreya Sharma",
+        duration:"03:40",
+        musicClass:"music-27415"
+
+    },
+    {
+        src:"https://pagalnew.com/download128/23014",
+        name:'Oo Bolega Ya Oo Oo Bolega',
+        singer: "Kanika Kapoor",
+        duration:"03:46",
+        musicClass:"music-23014"
+    },
+    {
+        src:"https://pagalnew.com/download128/20264",
+        name:'Srivali',
+        singer: "Javed Ali",
+        duration:"03:41",
+        musicClass:"music-20264"
+
+    },
+    {
+        src:"https://pagalnew.com/download128/23015",
+        name:'Saami Saami',
+        singer: "Sunidhi Chauhan",
+        duration:"03:47",
+        musicClass:"music-23015"
+
+    },
+    {
+        src:"https://pagalnew.com/download128/27415",
+        name:'Duppata',
+        singer: "Diesby, Shreya Sharma",
+        duration:"03:40",
+        musicClass:"music-27415"
+
+    },
 ]
