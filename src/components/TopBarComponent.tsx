09241f6af9320353b7db9c0f083167024acc41b4
Vivek Motwani <vivek.motwani0405@gmail.com>
2023-03-19 21:43:03 +0530

    logout function impl

--- src/components/TopBarComponent.tsx
+++ src/components/TopBarComponent.tsx
@@ -1,8 +1,15 @@
 import { FunctionComponent } from "react";
 import { Container, Image, Nav, Navbar, NavDropdown, Ratio } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import styles from "./TopBarComponent.module.css";
 
 const TopBarComponent: FunctionComponent = () => {
+    const navigate = useNavigate();
+    const logoutFunction = ()=>{
+        localStorage.removeItem("accessToken");
+        navigate("/");
+
+    }
     return (
         <Navbar className={styles.topbar} id="headerTopbar">
             <div className={styles.artboard134x81Parent} id="innerHeader">
@@ -27,8 +34,8 @@ const TopBarComponent: FunctionComponent = () => {
                         </NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">
-                            Separated link
+                        <NavDropdown.Item onClick={() => logoutFunction()}>
+                            Log out
                         </NavDropdown.Item>
                     </NavDropdown>
                 </div>
