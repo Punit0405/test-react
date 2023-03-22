b44e25c87c1fc1b6017c28c595569f1881e04884
Vivek Motwani <vivek.motwani0405@gmail.com>
2023-03-22 21:27:26 +0530

    feaure gallery page

--- src/pages/GalleryClientView.tsx
+++ src/pages/GalleryClientView.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Grid from '../components/Grid'
+import { Image } from 'react-bootstrap'
 
 const GalleryClientView = () => {
   return (
+    <>
+    <Image src='../../images11.jpg'  style={{maxHeight:"100%",maxWidth:"100%",objectFit:"cover"}}/>
     <Grid/>
+    </>
+    
   )
 }
 
