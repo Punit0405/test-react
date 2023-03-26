ed3b02f14e0da5eec41e98b520905a6a5d8a791d
CodeStar2244 <codestar2244@gmail.com>
2023-03-27 02:06:49 +0530

    billing done

---
+++ src/components/StudioManagement/BillingNav.tsx
@@ -0,0 +1,29 @@
+import { Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import styles from "./BillingNav.module.css"
+
+const BillingNav: any = () => {
+
+
+    return (
+        <Nav id="navbar" navbar variant="tabs" defaultActiveKey="studiomanagement" className={`${styles.navpadding}`}>
+            <Nav.Link as={NavLink} to="/dashboard" eventKey="dashboard">
+                <div className={styles.home} >
+                    <p>Branding</p>
+                </div>
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/gallery" eventKey="gallery" >
+                <div className={styles.home}>
+                    <p>Project</p>
+                </div>
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/studiomanagement/billing" eventKey="studiomanagement">
+                <div className={styles.home}>
+                    <p>Billing</p>
+                </div>
+            </Nav.Link>
+        </Nav>
+    );
+};
+
+export default BillingNav;
