681391f3cfef3c71477838e93e51b157d985fa84
Vivek Motwani <vivek.motwani0405@gmail.com>
2023-04-22 22:49:42 +0530

    asset registry get quote done

---
+++ src/components/AssetRegistry/InsuranceQuote.tsx
@@ -0,0 +1,29 @@
+import { useState} from "react";
+import AssetNavBar from "./AssetNavBar";
+import styles from "./InsuranceQuote.module.css";
+import { useNavigate } from "react-router-dom";
+
+
+const InsuranceQuoteComponent: any = () => {
+
+  
+
+  
+
+  return (
+    <section className={styles.rightcontainer}>
+      <AssetNavBar navTitle="Get Quote"/>
+
+      <div className={styles.requestQuoteDiv}>
+      <iframe frameBorder="0" 
+       style={{height:"600px",width:"99%",border:"none"}}
+      src='https://forms.zohopublic.com/snapeapp/form/InsuranceQuote/formperma/8l0zRSr5w8zz8ePcBK-Y8DhRhi8dhci7Tj62fG6c-YY'></iframe>
+
+      </div>
+
+
+    </section>
+  );
+};
+
+export default InsuranceQuoteComponent;
